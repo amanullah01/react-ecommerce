98dc7dacaab754d6243a7c85616696b6e1325af9
Amanullah Aman <amancse.aiub@gmail.com>
2019-12-27 21:31:49 +0600

    import React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/hompage.component";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>This is Hats page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact="true" path="/" component={HomePage} />
+      <Route exact="true" path="/hats" component={HatsPage} />
     </div>
   );
 }
