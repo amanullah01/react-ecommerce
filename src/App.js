27dddb1222306828faf1813f371478b80b55638b
Amanullah Aman <amancse.aiub@gmail.com>
2019-12-27 21:50:10 +0600

    Parameter pass via url and show it in view page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/hompage.component";
 
-const HatsPage = () => {
+const HatsPage = props => {
+  console.log(props);
   return (
     <div>
       <h1>This is Hats page</h1>
+    </div>
+  );
+};
+
+const TopicsList = () => {
+  return (
+    <div>
+      <h1>This is TopicsList page</h1>
+    </div>
+  );
+};
+const TopicDetail = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>This is details of # {props.match.params.topicId}</h1>
     </div>
   );
 };
@@ -17,7 +34,9 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/topics" component={TopicsList} />
+        <Route path="/topics/:topicId" component={TopicDetail} />
       </Switch>
     </div>
   );
